09354e4250049c1c5589a53d725e4d19f8318afb
Ksenia <lelsene@mail.ru>
2019-03-29 16:23:49 +0400

    Web: Step 1

---
+++ IceCreamShop/IceCreamShopWeb/FormBooking.aspx.cs
@@ -0,0 +1,116 @@
+﻿using IceCreamShopServiceDAL.BindingModels;
+using IceCreamShopServiceDAL.Interfaces;
+using IceCreamShopServiceDAL.ViewModels;
+using IceCreamShopServiceImplement.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace IceCreamShopWeb
+{
+    public partial class FormBooking : System.Web.UI.Page
+    {
+        private readonly ICustomerService serviceC = new CustomerServiceList();
+
+        private readonly IIceCreamService serviceS = new IceCreamServiceList();
+
+        private readonly IMainService serviceM = new MainServiceList();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                List<CustomerViewModel> listC = serviceC.GetList();
+                if (listC != null)
+                {
+                    DropDownListCustomer.DataSource = listC;
+                    DropDownListCustomer.DataBind();
+                    DropDownListCustomer.DataTextField = "CustomerName";
+                    DropDownListCustomer.DataValueField = "Id";
+                }
+                List<IceCreamViewModel> listP = serviceS.GetList();
+                if (listP != null)
+                {
+                    DropDownListIceCream.DataSource = listP;
+                    DropDownListIceCream.DataBind();
+                    DropDownListIceCream.DataTextField = "IceCreamName";
+                    DropDownListIceCream.DataValueField = "Id";
+                }
+                Page.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        private void CalcSum()
+        {
+
+            if (DropDownListIceCream.SelectedValue != null && !string.IsNullOrEmpty(TextBoxCount.Text))
+            {
+                try
+                {
+                    int id = Convert.ToInt32(DropDownListIceCream.SelectedValue);
+                    IceCreamViewModel product = serviceS.GetElement(id);
+                    int count = Convert.ToInt32(TextBoxCount.Text);
+                    TextBoxSum.Text = (count * product.Price).ToString();
+                }
+                catch (Exception ex)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void DropDownListService_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CalcSum();
+        }
+
+        protected void TextBoxCount_TextChanged(object sender, EventArgs e)
+        {
+            CalcSum();
+        }
+
+        protected void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxCount.Text))
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Заполните поле Количество');</script>");
+                return;
+            }
+            if (DropDownListCustomer.SelectedValue == null)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Выберите клиента');</script>");
+                return;
+            }
+            if (DropDownListIceCream.SelectedValue == null)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Выберите изделие');</script>");
+                return;
+            }
+            try
+            {
+                serviceM.CreateBooking(new BookingBindingModel
+                {
+                    CustomerId = Convert.ToInt32(DropDownListCustomer.SelectedValue),
+                    IceCreamId = Convert.ToInt32(DropDownListIceCream.SelectedValue),
+                    Count = Convert.ToInt32(TextBoxCount.Text),
+                    Sum = Convert.ToInt32(TextBoxSum.Text)
+                });
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Сохранение прошло успешно');</script>");
+                Server.Transfer("FormMain.aspx");
+            }
+            catch (Exception ex)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("FormMain.aspx");
+        }
+    }
+}
---
+++ IceCreamShop/IceCreamShopWeb/FormMain.aspx.cs
@@ -0,0 +1,106 @@
+﻿using IceCreamShopServiceDAL.BindingModels;
+using IceCreamShopServiceImplement.Implementations;
+using IceCreamShopServiceDAL.Interfaces;
+using IceCreamShopServiceDAL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IceCreamShopWeb
+{
+    public partial class FormMain : System.Web.UI.Page
+    {
+        private readonly IMainService service = new MainServiceList();
+
+        List<BookingViewModel> list;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                list = service.GetList();
+                dataGridView1.Columns[0].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void ButtonCreateBooking_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("FormBooking.aspx");
+        }
+
+        protected void ButtonTakeBookingInWork_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedIndex >= 0)
+            {
+                try
+                {
+                    //string index = list[dataGridView1.SelectedIndex].Id.ToString();
+                    //Session["id"] = index;
+                    int id = list[dataGridView1.SelectedIndex].Id;
+                    service.TakeBookingInWork(new BookingBindingModel { Id = id });
+                    LoadData();
+                    Server.Transfer("FormMain.aspx");
+                    //Server.Transfer("TakeBookingInWork.aspx");
+                }
+                catch (Exception ex)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void ButtonFinishBooking_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedIndex >= 0)
+            {
+                int id = list[dataGridView1.SelectedIndex].Id;
+                try
+                {
+                    service.FinishBooking(new BookingBindingModel { Id = id });
+                    LoadData();
+                    Server.Transfer("FormMain.aspx");
+                }
+                catch (Exception ex)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void ButtonBookingPayed_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedIndex >= 0)
+            {
+                int id = list[dataGridView1.SelectedIndex].Id;
+                try
+                {
+                    service.PayBooking(new BookingBindingModel { Id = id });
+                    LoadData();
+                    Server.Transfer("FormMain.aspx");
+                }
+                catch (Exception ex)
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void ButtonUpd_Click(object sender, EventArgs e)
+        {
+            LoadData();
+            Server.Transfer("FormMain.aspx");
+        }
+    }
+}
